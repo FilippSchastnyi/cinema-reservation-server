2b1a465567b83a376cd34170b3b16c30838e1014
Pilip Shchasny <filippshchasny@gmail.com>
2023-01-21 21:42:46 +0100

    CRS-pshchasny-created-registration-fu

--- src/graphQL/resolvers/userResolver.js
+++ src/graphQL/resolvers/userResolver.js
@@ -1,44 +1,68 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 import UserSchema from "../../models/UserSchema.js";
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
+
+async function isEmailDuplicated(email) {
+  return await UserSchema.countDocuments({email}) > 0
+}
+
+async function generateToken(payload) {
+  return await jwt.sign(payload, process.env.SECRET_KEY, {expiresIn: '24h'});
+}
 
 const userResolver = {
-    Query: {
-        async getAllUsers() {
-            return UserSchema.find();
-        },
-        async getUser(_, {id}) {
-            return UserSchema.findById(id);
-        },
+  Query: {
+    async getAllUsers() {
+      return UserSchema.find();
     },
-    Mutation: {
-        async createUser(_, {input}) {
-            return UserSchema.create(input);
-        },
+    async getUser(_, {id}) {
+      return UserSchema.findById(id);
+    },
+  },
+  Mutation: {
+    async createUser(_, {input}) {
+      return UserSchema.create(input);
+    },
 
-        async registerUser(_, {input}){
-            // check old user
+    async registerUser(_, {input}) {
+      const {email, password} = input
 
-            // throw Error if so
-
-            // encrypt password
-
-            // build mongoose model
-
-            // create JWT
-
-            // save user
-        },
-
-        async loginUser(_, {input}){
-            // if exist?
-                  // error if not
-            // if entered pass match the encrypted
-                  // error if not
-
-            // create new Token
-
-            // update token
+      if (await isEmailDuplicated(email)) {
+        return {
+          __typename: 'DuplicatedUser',
+          message: 'Email is duplicated',
         }
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 5).then(result => result)
+      const newUserData = {email, hashedPassword}
+      const token = await generateToken(newUserData)
+      UserSchema.create(newUserData); //::TODO
+      return {
+        __typename: "RegisteredUser",
+        message: "User has been registered",
+        user: {
+          email,
+          password: hashedPassword,
+        },
+        token
+      }
     },
+
+    async loginUser(_, {input}) {
+      // if exist?
+      // error if not
+      // if entered pass match the encrypted
+      // error if not
+
+      // create new Token
+
+      // update token
+    }
+  },
 }
 
 export default userResolver
--- src/models/UserSchema.js
+++ src/models/UserSchema.js
@@ -3,6 +3,7 @@ import { model, Schema } from 'mongoose'
 const userSchema = new Schema({
     email: {type: String, unique: true, required: true},
     password: {type: String, required: true},
+    roles: [{type:String, ref:"Role"}]
 })
 
 export default model('User', userSchema)
